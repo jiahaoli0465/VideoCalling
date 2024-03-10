ae29f2980d5ea67bd5673b611db5d51ccee7b00b
Jiahao Li <jiahaoli0465@gmail.com>
2024-03-10 00:51:32 -0500

    setting up socket connections

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,24 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import cors from "cors";
 
 const port = 8080;
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
-io.on("connection", () => {
+io.on("connection", (socket) => {
   console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 server.listen(port, () => {
