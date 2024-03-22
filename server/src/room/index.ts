050aba1a8212d861648eae31689daac24f47aa53
Jiahao Li <jiahaoli0465@gmail.com>
2024-03-22 02:05:08 -0400

    continuing with implementiation of the rooms

--- server/src/room/index.ts
+++ server/src/room/index.ts
@@ -1,16 +1,21 @@
 import { Socket } from "socket.io";
 import { v4 as uuidv4 } from "uuid";
 
+const rooms: Record<string, string[]> = {};
+
 export const roomHandler = (socket: Socket) => {
+  const createRoom = () => {
+    const roomId = uuidv4();
+    rooms[roomId] = [];
+    socket.emit("room-created", { roomId });
+    console.log("user created room");
+  };
+
   const joinRoom = ({ roomId }: { roomId: string }) => {
     console.log("user joined room", roomId);
     socket.join(roomId);
   };
-  const createRoom = () => {
-    const roomId = uuidv4();
-    socket.emit("room-created", { roomId });
-    console.log("user created room");
-  };
+
   socket.on("create-room", createRoom);
 
   socket.on("join-room", joinRoom);
